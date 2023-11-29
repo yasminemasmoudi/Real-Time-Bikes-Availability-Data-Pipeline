a0e3d868ff42ddd6942001de563be8e12e8a8f9b
yasminemasmoudi <yasminmasmoudi0902@gmail.com>
2023-11-29 18:52:36 -0500

    modifs

--- consumer-spark.py
+++ consumer-spark.py
@@ -1,89 +1,60 @@
+
 from pyspark.sql import SparkSession
-from pyspark import SparkContext
-from pyspark.sql.types import StringType
-import time
-from pyspark.sql.functions import from_json
-from pyspark.sql.types import StructType, StructField, BooleanType, IntegerType, MapType, FloatType
-import pickle 
-import json
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
 from elasticsearch import Elasticsearch
-es = Elasticsearch()
+es = Elasticsearch("http://localhost:9200")
 
-
-sc = SparkContext.getOrCreate()
-
-path = "/home/Documents/bikes/velo.csv"
-check_path = "/home/Documents/bikes/"
-
-
-spark = SparkSession.builder.appName('DataStreamingApp').getOrCreate()
+spark = SparkSession.builder.appName("consumer").getOrCreate()
 spark.sparkContext.setLogLevel("ERROR")
 
-chema = StructType(
-[StructField("number", IntegerType(), True),
- StructField("contract_name", StringType(), True),
- StructField("name", StringType(), True),
- StructField("address", StringType(), True),
- StructField("position",MapType(FloatType(), FloatType(), True)),
- StructField("banking", BooleanType(), True),
- StructField("bonus", BooleanType(), True),
- StructField("bike_stands", IntegerType(), True),
- StructField("available_bike_stands", IntegerType(), True),
- StructField("available_bikes", IntegerType(), True),
- StructField("status", StringType(), True),
- StructField("last_update", IntegerType(), True)])
- 
-df = spark \
-  .readStream \
-  .format("kafka") \
-  .option("kafka.bootstrap.servers", "localhost:9092") \
-  .option("subscribe", "velib-topic") \
-  .load()
-print(df.isStreaming)
+schema = StructType([
+    StructField("numbers", IntegerType(), True),
+    StructField("contract_name", StringType(), True),
+    StructField("banking", StringType(), True),
+    StructField("bike_stands", IntegerType(), True),
+    StructField("available_bike_stands", IntegerType(), True),
+    StructField("available_bikes", IntegerType(), True),
+    StructField("address", StringType(), True),
+    StructField("status", StringType(), True),
+    StructField("position", StructType([
+        StructField("lat", DoubleType(), True),
+        StructField("lng", DoubleType(), True)
+    ]), True),
+    StructField("timestamps", StringType(), True),
+])
+
+kafka_df = spark \
+    .readStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", "localhost:9092") \
+    .option("subscribe", "bike") \
+    .option("startingOffsets", "latest")\
+    .load()
+
+json_df = kafka_df.selectExpr("CAST(value AS STRING)") \
+    .select(from_json("value", schema).alias("data")) \
+    .select("data.*")
+json_df = json_df.withColumn("position", col("position").alias("position").cast("struct<lat:double, lon:double>"))
+
+zero_bikes_df = json_df.filter(col("available_bikes") == 0)
 
 
-parsed_df = df \
-            .select(from_json(col('value').cast('string'),
-                    chema).alias('parsed_value')) \
-            .withColumn('timestamp', lit(current_timestamp()))
-mdf = parsed_df.select('parsed_value.*', 'timestamp')
-print(mdf)
-
-    
-#query.awaitTermination() 
-
-split_col =  split(df.value, '\\,')
-
-df = df.withColumn("number", split_col.getItem(0))\
-    .withColumn("contract_name", split_col.getItem(1))\
-    .withColumn("name", split_col.getItem(2))\
-    .withColumn("address", split_col.getItem(3))\
-    .withColumn("position", split_col.getItem(4))\
-    .withColumn("banking", split_col.getItem(5))\
-    .withColumn("bonus", split_col.getItem(6))\
-    .withColumn("available_bike_stands", split_col.getItem(7))\
-    .withColumn("available_bikes", split_col.getItem(8))\
-    .withColumn("status", split_col.getItem(9))\
-    .withColumn("last_update", split_col.getItem(10))\
-    .drop("key")
-
-print(df)
-
-query = df \
-    .selectExpr("number" , "contract_name" , "name" , "address" , "position" , "banking" , "bonus" , "available_bike_stands" , "available_bikes" , "status" , "last_update") \
+query = zero_bikes_df \
     .writeStream \
+    .outputMode("append") \
     .format("console") \
-    .option("header" , True) \
     .start()
 
-query = df.writeStream.format('consol').option("es.port", "9200").option("es.nodes" , "localhost").option("es.resource", "yass").outputMode("append").start()
-  
-query.awaitTermination()
-print("yes")
 
-
-
-
-
+data = json_df.writeStream \
+        .format("org.elasticsearch.spark.sql") \
+        .outputMode("append")\
+        .option("es.nodes", "127.0.0.1")\
+        .option("es.port", "9200")\
+        .option("es.index.auto.create", "true") \
+        .option("es.resource", "bike")\
+        .option("es.nodes.wan.only", "true") \
+        .option("checkpointLocation", "/home/osboxes/Documents/projects/bikes/checkpoints/new") \
+        .start()
+data.awaitTermination()
--- producer-kafka.py
+++ producer-kafka.py
@@ -1,19 +1,54 @@
+from kafka import KafkaProducer
 import json
 import time
-import urllib.request
-from kafka import KafkaProducer
+from datetime import datetime
+import requests
 
-API_KEY = "62ebd294a838133f3a07500d5a40e74603e23d82" 
-url = "https://api.jcdecaux.com/vls/v1/stations?apiKey={}".format(API_KEY)
+producer = KafkaProducer(bootstrap_servers=['localhost:9092'], value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
-producer = KafkaProducer(bootstrap_servers="localhost:9092")
+api_key = "62ebd294a838133f3a07500d5a40e74603e23d82"
+base_url = 'https://api.jcdecaux.com/vls/v1/'
+endpoint = 'stations'
+country_code = 'FR'
 
-while True:
-    response = urllib.request.urlopen(url)
-    stations = json.loads(response.read().decode())
-    for station in stations:
-        producer.send("velib-topic", json.dumps(station).encode())
-        print(station)
+while True:  # Infinite loop for continuous streaming, you may adjust this as needed
+    try:
+        url = f'{base_url}{endpoint}?country_code={country_code}&apiKey={api_key}'
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            data = response.json()
+
+            for line in data:
+                utcfromtimestamp = datetime.utcfromtimestamp(int(line['last_update'])/1000).strftime('%Y-%m-%d %H:%M:%S')
+
+                d = {
+                    'numbers': line['number'],
+                    'contract_name': line['contract_name'],
+                    'banking': line['banking'],
+                    'bike_stands': line['bike_stands'],
+                    'available_bike_stands': line['available_bike_stands'],
+                    'available_bikes': line['available_bikes'],
+                    'address': line['address'],
+                    'status': line['status'],
+                    'position':line['position'],
+                    'timestamps': utcfromtimestamp
+                }
+
+                producer.send('bike', value=d)
+                print(d)
+                time.sleep(2)
+                
+            
+        else:
+            print(f'Error: {response.status_code}')
+            print(response.text)
+
+    except Exception as e:
+        print(f'An error occurred: {str(e)}')
+
+  
+    time.sleep(60)  
         
 
     
